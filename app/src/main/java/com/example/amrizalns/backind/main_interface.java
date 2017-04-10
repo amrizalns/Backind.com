742cfbdc8bd93b11607ee68c0fd7655c6d0f0c76
Aditya Shofwan Zulma <adityazulma@live.com>
2017-04-10 21:17:26 +0700

    1. Menambahkan google play services 2. Menambahkan manifest untuk keperluan google maps 3. Menambahkan API Key sesuai SHA1 aplikasi Backind 4. Menambahkan mapsLocation.java > untuk mengetahui current location 5. Menambahkan PermissionUtils.java 6. Debugging beberapa code yang error

--- app/src/main/java/com/example/amrizalns/backind/main_interface.java
+++ app/src/main/java/com/example/amrizalns/backind/main_interface.java
@@ -1,5 +1,6 @@
 package com.example.amrizalns.backind;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class main_interface extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
