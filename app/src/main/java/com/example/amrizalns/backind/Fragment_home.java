dda069df5717bb7707b8ab1ee1f235d96d66bef7
amrizalns <amrizalns@gmail.com>
2017-04-17 10:43:48 +0700

    Dubuging

--- app/src/main/java/com/example/amrizalns/backind/Fragment_home.java
+++ app/src/main/java/com/example/amrizalns/backind/Fragment_home.java
@@ -22,7 +22,7 @@ public class Fragment_home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.content_main_interface, container, false);
+        View view =  inflater.inflate(R.layout.fragment_dashboard, container, false);
         return view;
     }
 }
--- app/src/main/java/com/example/amrizalns/backind/Fragment_maps.java
+++ app/src/main/java/com/example/amrizalns/backind/Fragment_maps.java
@@ -6,6 +6,7 @@ package com.example.amrizalns.backind;
 
 import android.Manifest;
 import android.app.Fragment;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +37,7 @@ public class Fragment_maps extends android.support.v4.app.Fragment implements On
     private GoogleMap mMap;
     private MapView mapView;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu_maps, container, false);
@@ -49,30 +52,47 @@ public class Fragment_maps extends android.support.v4.app.Fragment implements On
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
         mapView.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        {
 
-
-        }
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
+//            if(gps.canGetLocation()){
+//
+//                double lat = gps.getLatitude();
+//                double lang = gps.getLongitude();
+//
+//                // \n is for new line
+//                Log.i("Lat", lat+"");
+//                Log.i("Lang", lang+"");
+////            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+//            }else{
+//                // can't get location
+//                // GPS or Network is not enabled
+//                // Ask user to enable GPS/network in settings
+//                gps.showSettingsAlert();
+//            }
+//
+//            double lat = gpsTracker.getLatitude();
+//            double lang = gpsTracker.getLongitude();
+//            Log.i("Lat", lat+"");
+//            Log.i("Lang", lang+"");
+//            LatLng latLng = new LatLng(lat, lang);
             return;
         }
 
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
 
-        LatLng latlng = new LatLng(-6.974001, 107.630434);
 
 
         mMap.setTrafficEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 7));
-        mMap.addMarker(new MarkerOptions().position(latlng).title(""));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 7));
+//        mMap.addMarker(new MarkerOptions().position(latlng).title(""));
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
 
             @Override
